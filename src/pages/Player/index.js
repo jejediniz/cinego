a5d0b8df50bae164dbfa68af59432649de725dfd
jejediniz <jessicadiniz1224@gmail.com>
2025-12-09 16:49:38 -0300

    Atualizando parâmetros

--- src/pages/Player/index.js
+++ src/pages/Player/index.js
@@ -2,14 +2,20 @@ import Banner from 'components/Banner';
 import styles from './Player.module.css';
 import Titulo from 'components/Titulo';
 import { useParams } from 'react-router-dom';
-import videos from 'json/db.json';
 import NaoEncontrada from 'pages/NaoEncontrada';
+import { useEffect, useState } from 'react';
 
 function Player() {
+  const [video, setVideos] = useState ();
     const parametros = useParams();
-    const video = videos.find((video) => {
-        return video.id === Number(parametros.id);
-    })
+
+    useEffect (() => {
+      fetch(`https://my-json-server.typicode.com/monicahillman/cinetag-api/videos?id=${parametros.id}`)
+      .then(resposta => resposta.json())
+      .then(dados => {
+        setVideos(...dados);
+      })
+    },[])
 
     if (!video) {
         return <NaoEncontrada />;
